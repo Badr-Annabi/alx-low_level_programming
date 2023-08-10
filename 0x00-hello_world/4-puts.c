49199140c36a6380c52ac549a363c5b4c8bd307f
Badr_Annabi <badrannabi@gmail.com>
2023-08-10 07:41:18 -0700

    TASK 4 printing a sentence using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
  * main :i used it to print the sentence below using put
- * Return : 0
+ * Return : return 0
  */
-int main (void) {
+int main(void)
+{
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
