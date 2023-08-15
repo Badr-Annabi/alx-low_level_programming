41e482f8132f2485459a666099f9a29bbab81471
Badr_Annabi <badrannabi@gmail.com>
2023-08-15 08:43:41 -0700

    TASK 1 _putchar alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - prints the alphabet, in lowercase
-* Return: Always (0) Success
+* Return: void
 */
 void print_alphabet(void)
 {
@@ -11,6 +11,5 @@ void print_alphabet(void)
 	{
 		_putchar(i);
 	}
-	putchar('\n');
-	return (0);
+	_putchar('\n');
 }
