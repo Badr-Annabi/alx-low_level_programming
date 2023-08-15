4fc80508907dc034d383bbc8ee6e341b219d241e
Badr_Annabi <badrannabi@gmail.com>
2023-08-15 09:23:57 -0700

    TASK 4 check alphabet character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabetic character
+* @c: the character that we check
+* Return: 1 if c lowercase or uppercase, 0 if not
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
