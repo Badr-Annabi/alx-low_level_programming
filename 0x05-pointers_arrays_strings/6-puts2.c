642a64e4656bd00867b18f05acf8bec776093fa8
Badr_Annabi <badrannabi@gmail.com>
2023-08-21 15:17:25 -0700

    TASK 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* puts2 - prints every other character of a string
+* @str: input
+*/
+void puts2(char *str)
+{
+	int i;
+	int l;
+	char *s = str;
+
+	while (*s != '\0')
+	{
+		l++;
+	}
+	for (i = 0; i < l - 1; i++)
+	{
+		if (i % 2 == 0)
+		{
+		_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
