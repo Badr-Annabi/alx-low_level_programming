b6bff9df5b36c3adc3cc4599d2e49fcb1b69e8fd
Badr_Annabi <badrannabi@gmail.com>
2023-08-23 17:29:04 -0700

    TASK 2 strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strncpy - copies a string.
+* @dest: destination
+* @src: source
+* @n: number of digit that we want to copy.
+* Return: character.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
