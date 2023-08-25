6debd80415984fe5f1a27fae8298d7c864556a92
Badr_Annabi <badrannabi@gmail.com>
2023-08-24 19:50:22 -0700

    TASK 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,50 +7,45 @@
  * @size_r: buffer size
  * Return: String with all letters in ROT13 base.
  */
+#include <stdio.h>
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int a = 0, b = 0, c = 0, a1, b1, sum, big;
 
-	while (n1[a] != '\0' && n2[b] != '\0')
-	{
+	while (n1[a] != '\0')
 		a++;
+	while (n2[b] != '\0')
 		b++;
-	}
 	if (a > b)
 		big = a;
 	else
 		big = b;
-	if ((big + 1) >= size_r)
-		return (0);
+	if ((big + 1) > size_r)
+	{return NULL; }
 	r[big + 1] = '\0';
-	while (big >= 0)
+	while (big > 0)
 	{
-		a1 = (n1[a - 1] - '0');
-		b1 = (n1[b - 1] - '0');
-		if (a > 0 && b > 0)
-			sum = a1 + b1 + c;
-		else if (a < 0 && b > 0)
-			sum = b1 + c;
-		else if (a > 0 && b < 0)
-			sum = a1 + c;
-		else
-			sum = c;
-		if (sum > 9)
-		{	c = sum / 10;
-			sum = (sum % 10) + '0';
-		} 
-		else
+		a1 = (a > 0) ? (n1[a - 1] - '0') : 0;
+		b1 = (b > 0) ? (n2[b - 1] - '0') : 0;
+
+		sum = a1 + b1 + c;
+		c = sum / 10;
+		sum = sum % 10 + '0';
+		r[big - 1] = sum;
+		if (a > 0)
+			a--;
+		if (b > 0)
 		{
-			c = 0;
-			sum = sum + '0';
+			b--;
+			big--;
 		}
-		r[big] = sum;
-		a1--;
-		b1--;
-		big--;
 	}
-	if (*(r) != 0)
-		return (r);
-	else
-		return (r + 1);
+	if (c > 0)
+	{
+		r[0] = c + '0';
+		{return r; }
+	}
+
+	{return r + 1; }
 }
