456e397b990264a59eefde5ec498e259f36f1caa
Badr_Annabi <badrannabi@gmail.com>
2023-08-25 08:34:03 -0700

    TASK 11 infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,77 @@
 #include "main.h"
-/**
- * infinite_add -  adds two numbers.
- * @n1: first string.
- * @n2: second string
- * @r: buffer
- * @size_r: buffer size
- * Return: String with all letters in ROT13 base.
- */
-#include <stdio.h>
 
+/**
+ * rev_string - reverse an array
+ * @n: char n
+ * Return: zero
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char x;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		x = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = x;
+	}
+}
+
+/**
+ * infinite_add - add two numbers.
+ * @n1: char string
+ * @n2: char string
+ * @r: char string
+ * @size_r: int
+ * Return: pointer
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a = 0, b = 0, c = 0, a1, b1, sum, big;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int v1 = 0, v2 = 0, x = 0;
 
-	while (n1[a] != '\0')
-		a++;
-	while (n2[b] != '\0')
-		b++;
-	if (a > b)
-		big = a;
-	else
-		big = b;
-	if ((big + 1) > size_r)
-	{return NULL; }
-	r[big + 1] = '\0';
-	while (big > 0)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		a1 = (a > 0) ? (n1[a - 1] - '0') : 0;
-		b1 = (b > 0) ? (n2[b - 1] - '0') : 0;
-
-		sum = a1 + b1 + c;
-		c = sum / 10;
-		sum = sum % 10 + '0';
-		r[big - 1] = sum;
-		if (a > 0)
-			a--;
-		if (b > 0)
-		{
-			b--;
-			big--;
-		}
+		if (i < 0)
+			v1 = 0;
+		else
+			v1 = *(n1 + i) - '0';
+		if (j < 0)
+			v2 = 0;
+		else
+			v2 = *(n2 + j) - '0';
+		x = v1 + v2 + overflow;
+		if (x >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (x % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (c > 0)
-	{
-		r[0] = c + '0';
-		{return r; }
-	}
-
-	{return r + 1; }
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
