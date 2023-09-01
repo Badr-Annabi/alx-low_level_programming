9f6c410d8403de92656081b591846f733c919ca4
Badr_Annabi <badrannabi@gmail.com>
2023-09-01 12:02:56 -0700

    TASK 0 testing argv and see what it does

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints its name, followed by a new line.
+ * @argv: first argument argc.
+ * @argv: second arguments argv.
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n",*argv);
+	return (0);
+}
