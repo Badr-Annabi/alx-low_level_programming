c9db3cb4f4db76794c41db58d9cb032a1bd72665
Badr_Annabi <badrannabi@gmail.com>
2023-09-04 07:26:14 -0700

    TASK 1 PUT a string in an allocated array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated
+ * space in memory, which contains a copy
+ * of the string given as a parameter.
+ * @str: the string that we want to put in the allocated
+ * array.
+ * Return: the string that we want.
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *ptr;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	ptr = malloc(sizeof(char) * (len));
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
