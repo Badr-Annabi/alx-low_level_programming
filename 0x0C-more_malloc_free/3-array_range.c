5a2f8a17b66ada6d0a6d330b9e7239c7e81cbbaf
Badr_Annabi <badrannabi@gmail.com>
2023-09-07 17:06:08 -0700

    TASK 3 Displaying array from min to max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers.
+ * @min: the integer thta we ll start from.
+ * @max: the last integer.
+ * Return: ptr (which is int).
+ */
+int *array_range(int min, int max)
+{
+	int *ptr, i, diff = 0;
+
+	if (min > max)
+		return (NULL);
+	diff = max - min + 1;
+
+	ptr = malloc(sizeof(int) * diff);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= max; i++)
+		ptr[i] = min + i;
+	return (ptr);
+
+
+}
