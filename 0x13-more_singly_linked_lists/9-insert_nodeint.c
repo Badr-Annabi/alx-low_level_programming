29509e850abcd07cbb09a7c40ebf553d204af84a
Badr_Annabi <badrannabi@gmain.com>
2023-09-26 06:37:23 -0700

    TASK 9 insert node in the propper index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index -  inserts a new node at a given position.
+ * @head: Pointer to the first element.
+ * @idx: the index of the list where the new node should be added.
+ * Index starts at 0.
+ * @n: the value of the new node.
+ * Return:  the address of the new node, or NULL if it failed.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *node;
+	unsigned int i = 0;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (!head || !new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!idx)
+	{
+		new_node = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	node = *head;
+	while (node)
+	{
+		if (i == idx - 1)
+		{
+			new_node->next = node->next;
+			node->next = new_node;
+			return (new_node);
+		}
+		i++;
+		node = node->next;
+	}
+	free(new_node);
+	return (NULL);
+}
