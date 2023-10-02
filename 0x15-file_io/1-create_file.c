bffae230f45d22bd9b7365de75ecd9a8328bf715
Badr_Annabi <Badrannabi@gmail.com>
2023-10-02 14:12:28 -0700

    TASK 1 Creating a file with its content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	while (text_content[len])
+	while (text_content[len] != '\0')
 		len++;
+
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
