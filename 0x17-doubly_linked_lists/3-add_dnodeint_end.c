90f49af7d64d6215baf79f0edc76f2e338af9119
Badr_Annabi <badrannabi@gmail.com>
2023-11-09 17:21:48 +0100

    TADSK 3 add new node to the end of the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,8 +28,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		node = *head;
 		while (node->next)
 			node = node->next;
-		(node)->next = new_node;
-		new_node->prev = *head;
+		node->next = new_node;
+		new_node->prev = node;
 	}
 	return (new_node);
 }
