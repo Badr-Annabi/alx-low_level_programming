bb5632a21163b3843a3255d7c8aff78bbdbf579c
Badr_Annabi <badrannabi@gmail.com>
2023-11-30 21:32:17 +0100

    add return null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,9 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
+	if (!table)
+		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t **) calloc(
+	table->array = (hash_node_t **)calloc(
 			table->size, sizeof(hash_node_t **));
+	if (!table->array)
+		return (NULL);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
